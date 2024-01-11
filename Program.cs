eb850132a6450122ee9571545d579542ab9a4552
Leonardo Santos <leonardosantos060804@gmail.com>
2024-01-11 16:26:23 -0300

    Implementações das classes concluídasgit add .

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "11976405388", modelo: "8", imei: "222222222", memoria: 80);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-----------------------");
+
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "11976405388", modelo: "8", imei: "222222222", memoria: 80);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
