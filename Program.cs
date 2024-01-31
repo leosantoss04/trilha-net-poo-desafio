a321dc46bb7c130e61dc3de5d66eb1e01f7b5f29
leosantoss04 <148256610+leosantoss04@users.noreply.github.com>
2024-01-31 20:58:14 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 Console.WriteLine("Smartphone Nokia:");
-Smartphone nokia = new Nokia(numero: "11976405388", modelo: "8", imei: "222222222", memoria: 80);
+Smartphone nokia = new Nokia(numero: "11976405388", modelo: "8", imei: "7", memoria: 80);
 nokia.Ligar();
 nokia.InstalarAplicativo("Whatsapp");
 
@@ -9,7 +9,7 @@ Console.WriteLine("-----------------------");
 
 
 Console.WriteLine("Smartphone Iphone:");
-Smartphone iphone = new Iphone(numero: "11976405388", modelo: "8", imei: "222222222", memoria: 80);
+Smartphone iphone = new Iphone(numero: "11976405388", modelo: "15", imei: "9", memoria: 95);
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Instagram");
 
